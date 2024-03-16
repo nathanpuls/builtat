f1316da2948d765c7431ea8a2edc30ee190745b8
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2024-03-15 22:13:17 -0500

    Update fx.js

--- cdn/fx.js
+++ cdn/fx.js
@@ -1,3 +1,23 @@
 function param(name) {
   return (new URLSearchParams(window.location.search)).get(name);
 }
+
+function copy(text) {
+    // Create a temporary input element
+    var tempInput = document.createElement("input");
+    
+    // Set the input value to the text you want to copy
+    tempInput.value = text;
+
+    // Append the input element to the document
+    document.body.appendChild(tempInput);
+
+    // Select the text inside the input element
+    tempInput.select();
+
+    // Execute the "copy" command to copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    // Remove the temporary input element
+    document.body.removeChild(tempInput);
+}
