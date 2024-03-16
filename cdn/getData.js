3f7eee2016bb4b9acd3b94562bcbdddf779560ca
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2024-03-16 10:09:20 -0500

    Update getData.js

--- cdn/getData.js
+++ cdn/getData.js
@@ -1,17 +1,22 @@
-   function getData(url, htmlTemplate) {
-     const containerId = 'data';
-  axios.get(url)
-    .then(response => {
-      const dataBox = document.getElementById(containerId);
-      const data = response.data;
+function getData(url, htmlTemplate) {
+    const containerId = 'data';
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const dataBox = document.getElementById(containerId);
 
-      data.forEach(item => {
-        const element = document.createElement('div');
-        element.innerHTML = htmlTemplate(item);
-        dataBox.appendChild(element);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+            data.forEach(item => {
+                const element = document.createElement('div');
+                element.innerHTML = htmlTemplate(item);
+                dataBox.appendChild(element);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
